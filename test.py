8f2979002dbf4ea2291af9a68a4ef4682e63217f
Alex <alexrandles0@gmail.com>
2023-04-03 13:32:23 +0100

    backup

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 from rdflib import *
+from collections import defaultdict
+graph_filename = "/home/alex/MQI-Framework/static/change_detection_cache/change_graphs/1.trig"
+change_graph = ConjunctiveGraph()
+change_graph.parse(graph_filename, format="trig")
 def analyse_mappings():
-    graph_filename = "/home/alex/MQI-Framework/static/change_detection_cache/change_graphs/1.trig"
-    change_graph = ConjunctiveGraph()
-    change_graph.parse(graph_filename, format="trig")
     # change_graph.parse("/home/alex/MQI-Framework/static/uploads/mapping.ttl", format="ttl")
     query = """
     PREFIX oscd: <https://w3id.org/OSCD#>
     PREFIX rml: <http://semweb.mmlab.be/ns/rml#>
     PREFIX rr: <http://www.w3.org/ns/r2rml#>
-    SELECT ?mappingGraph ?tripleMap ?source ?change ?reference ?changedData
+    SELECT ?mappingGraph ?tripleMap ?source ?change ?reference ?changedData ?changesGraph
     WHERE {
       GRAPH ?changesGraph {    			 
         ?changeLog a oscd:ChangeLog;
@@ -36,11 +37,66 @@ def analyse_mappings():
     change_graph.serialize(destination="test.trig", format="trig")
     qres = change_graph.query(query)
     matched_graphs = defaultdict(dict)
+    matched_graphs[mapping_graph_id] = defaultdict(dict)
+    matched_graphs[mapping_graph_id]["data_reference_changes"] = {
+        "insert": defaultdict(dict),
+        "delete": defaultdict(dict),
+    }
+    # data reference changes
+    current_data_reference_changes = matched_graphs[mapping_graph_id]["data_reference_changes"]
     for row in qres:
-        matched_graphs[mapping_graph_id] = {
-
-        }
-        print(row)
+        data_reference = row["reference"]
+        changed_data = row["changedData"]
+        change_identifier = row["change"]
+        change_type = "insert"
+        if "delete" in change_identifier:
+            change_type = "delete"
+        if data_reference in current_data_reference_changes[change_type]:
+            current_data_reference_changes[change_type][data_reference].append(changed_data)
+        else:
+            current_data_reference_changes[change_type][data_reference] = [changed_data]
+    # add structural changes if theres match
+    if current_data_reference_changes:
+        find_structural_changes(row["changesGraph"])
     return qres
 
+def find_structural_changes(graph_name):
+    print(graph_name)
+    query = """
+        PREFIX oscd: <https://w3id.org/OSCD#>
+        PREFIX rml: <http://semweb.mmlab.be/ns/rml#>
+        PREFIX rr: <http://www.w3.org/ns/r2rml#>
+
+        SELECT ?referenceChanged ?dataChanged
+        WHERE {
+          GRAPH <%s> {    			 
+            ?changeLog a oscd:ChangeLog;
+                    oscd:hasChange ?change . 
+            ?change oscd:hasDataReference ?referenceChanged;
+                    oscd:hasChangedData ?dataChanged .
+
+            {
+              SELECT ?referenceChanged ?stucturalReference
+              WHERE {
+                GRAPH <%s> {    			 
+                  ?changeLog a oscd:ChangeLog;
+                          oscd:hasChange ?change . 
+                  ?change oscd:hasStructuralReference ?stucturalReference;
+                          oscd:hasChangedData ?referenceChanged .
+
+                }
+             }
+
+            }
+
+          }
+        }
+    """ % (graph_name, graph_name)
+    qres = change_graph.query(query)
+    print(query)
+    for row in qres:
+        print(row)
+
 t = analyse_mappings()
+
+
