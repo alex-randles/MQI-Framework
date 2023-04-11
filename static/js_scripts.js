9699a0140107894c6a62a72fd77440c4727ef8a1
Alex <alexrandles0@gmail.com>
2023-04-11 16:52:47 +0100

    backup

--- static/js_scripts.js
+++ static/js_scripts.js
@@ -83,7 +83,7 @@ var i = 0;
     i = 1;
     var elem = document.getElementById("myBar");
     var width = 1;
-    var id = setInterval(frame, 400);
+    var id = setInterval(frame, 800);
     function frame() {
       if (width >= 100) {
        //  clearInterval(id);
